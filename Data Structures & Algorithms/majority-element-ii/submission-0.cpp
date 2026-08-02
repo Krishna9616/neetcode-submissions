4a33af97800a87672d9cf7ceeadf1f6a3a7f2aab
Krishna Kumar <125913723+Krishna9616@users.noreply.github.com>
2026-08-03 00:32:18 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int x : nums) mp[x]++;
+        for(auto x : mp){
+            if(x.second > n/3) ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
