ab3c193ec8571d44cb745a1beea862ec535f0929
Krishna Kumar <125913723+Krishna9616@users.noreply.github.com>
2026-08-04 18:27:04 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int l = 1, r = x;
+        int ans = 0;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (1LL * m * m == x)
+                return m;
+            else if (1LL * m * m < x) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+};
